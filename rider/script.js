57224748fa45d062a842e4365eb6c92176034018
epik8498 <epik8498@gmail.com>
2026-07-25 03:42:28 +0900

    Update script.js

--- rider/script.js
+++ rider/script.js
@@ -3,11 +3,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const startBtn = document.getElementById("startButton");
     const endBtn = document.getElementById("endButton");
 
+    const savedStatus = localStorage.getItem("workStatus");
+
+    if (savedStatus === "working") {
+        status.textContent = "출근 상태입니다.";
+    } else {
+        status.textContent = "퇴근 상태입니다.";
+    }
+
     startBtn.addEventListener("click", () => {
         status.textContent = "출근 상태입니다.";
+        localStorage.setItem("workStatus", "working");
     });
 
     endBtn.addEventListener("click", () => {
         status.textContent = "퇴근 상태입니다.";
+        localStorage.setItem("workStatus", "off");
     });
 });

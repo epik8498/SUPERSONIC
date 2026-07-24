16300764419481b2876a73103be3940b0b731a41
epik8498 <epik8498@gmail.com>
2026-07-25 04:18:54 +0900

    Update firebase.js

--- rider/firebase.js
+++ rider/firebase.js
@@ -1,21 +1,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
 import {
-  getAuth,
-  signInWithEmailAndPassword
+    getAuth,
+    signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 import {
-  getFirestore
+    getFirestore,
+    collection,
+    addDoc
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2IRDO33QufPwcr6qBfq35wPb7qSYLB3s",
-  authDomain: "supersonic77rider.firebaseapp.com",
-  projectId: "supersonic77rider",
-  storageBucket: "supersonic77rider.firebasestorage.app",
-  messagingSenderId: "443902968540",
-  appId: "1:443902968540:web:b8ec11866d8f258fa46a19"
+    apiKey: "AIzaSyA2IRDO33QufPwcr6qBfq35wPb7qSYLB3s",
+    authDomain: "supersonic77rider.firebaseapp.com",
+    projectId: "supersonic77rider",
+    storageBucket: "supersonic77rider.firebasestorage.app",
+    messagingSenderId: "443902968540",
+    appId: "1:443902968540:web:b8ec11866d8f258fa46a19"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -23,10 +25,19 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+async function saveAttendance(email) {
+    await addDoc(collection(db, "attendance"), {
+        email: email,
+        status: "출근",
+        time: new Date().toISOString()
+    });
+}
+
 export {
-  auth,
-  db,
-  signInWithEmailAndPassword
+    auth,
+    db,
+    signInWithEmailAndPassword,
+    saveAttendance
 };
 
 console.log("✅ Firebase + Firestore 연결 성공");

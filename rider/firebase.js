672b73ce5eb04f0c306c05696fe0a3b5b662d53d
epik8498 <epik8498@gmail.com>
2026-07-25 03:54:54 +0900

    Update firebase.js

--- rider/firebase.js
+++ rider/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "여기에 apiKey",
-  authDomain: "여기에 authDomain",
-  projectId: "여기에 projectId",
-  storageBucket: "여기에 storageBucket",
-  messagingSenderId: "여기에 messagingSenderId",
-  appId: "여기에 appId"
+  apiKey: "AIzaSyA2IRDO33QufPwcr6qBfq35wPb7qSYLB3s",
+  authDomain: "supersonic77rider.firebaseapp.com",
+  projectId: "supersonic77rider",
+  storageBucket: "supersonic77rider.firebasestorage.app",
+  messagingSenderId: "443902968540",
+  appId: "1:443902968540:web:b8ec11866d8f258fa46a19"
 };
 
 const app = initializeApp(firebaseConfig);
 
-console.log("Firebase 연결 완료");
+console.log("✅ Firebase 연결 성공");

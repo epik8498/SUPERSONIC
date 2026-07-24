b8f7d131ad1b9d65be5e93cf6538339cddc56e14
epik8498 <epik8498@gmail.com>
2026-07-25 04:11:47 +0900

    Update script.js

--- rider/script.js
+++ rider/script.js
@@ -4,6 +4,10 @@ import {
 } from "./firebase.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    const loginBox = document.getElementById("login-box");
+    const app = document.getElementById("app");
+
     const status = document.getElementById("workStatus");
     const startBtn = document.getElementById("startButton");
     const endBtn = document.getElementById("endButton");
@@ -11,6 +15,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const emailInput = document.getElementById("email");
     const passwordInput = document.getElementById("password");
     const loginBtn = document.getElementById("loginBtn");
+
+    app.style.display = "none";
 
     const savedStatus = localStorage.getItem("workStatus");
 
@@ -31,22 +37,32 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     loginBtn.addEventListener("click", async () => {
+
         const email = emailInput.value.trim();
         const password = passwordInput.value.trim();
 
-        if (email === "" || password === "") {
-            alert("이메일과 비밀번호를 모두 입력해주세요.");
+        if (!email || !password) {
+            alert("이메일과 비밀번호를 입력해주세요.");
             return;
         }
 
         try {
+
             await signInWithEmailAndPassword(auth, email, password);
 
-            alert("로그인에 성공했습니다.");
+            loginBox.style.display = "none";
+            app.style.display = "block";
+
+            alert("로그인 성공");
+
         } catch (error) {
+
+            alert("로그인 실패");
+
             console.error(error);
 
-            alert("로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.");
         }
+
     });
+
 });
